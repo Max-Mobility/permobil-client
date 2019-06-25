ccdb36e2badb5faf1a085b3c6c8d369e2b1e29a6
Brad Martin <bradwaynemartin@gmail.com>
2019-06-25 10:43:44 -0500

    app resource icon enum

---
+++ apps/mobile/pushtracker/src/app/enums/app-resource-icons.ts
@@ -0,0 +1,9 @@
+export enum AppResourceIcons {
+  'HOME_ACTIVE' = 'res://home_active',
+  'HOME_INACTIVE' = 'res://home_inactive',
+  'JOURNEY_ACTIVE' = 'res://journey_active',
+  'JOURNEY_INACTIVE' = 'res://journey_inactive',
+  'PROFILE_ACTIVE' = 'res://profile_active',
+  'PROFILE_INACTIVE' = 'res://profile_inactive',
+  'WHEELCHAIR_BLACK' = 'res://accessible_forward_black'
+}
--- apps/mobile/pushtracker/src/app/enums/index.ts
+++ apps/mobile/pushtracker/src/app/enums/index.ts
@@ -1,2 +1,3 @@
+export * from './app-resource-icons';
 export * from './storage-keys';
 
