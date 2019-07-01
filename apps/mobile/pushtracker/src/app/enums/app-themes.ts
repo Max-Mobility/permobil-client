7b866988cce2b0253e86367f92dd3356e5c15d20
Brad Martin <bradwaynemartin@gmail.com>
2019-07-01 10:55:10 -0700

    app theme enum

---
+++ apps/mobile/pushtracker/src/app/enums/app-themes.ts
@@ -0,0 +1,4 @@
+export enum APP_THEMES {
+  'DEFAULT' = 'DEFAULT',
+  'DARK' = 'DARK'
+}
--- apps/mobile/pushtracker/src/app/enums/index.ts
+++ apps/mobile/pushtracker/src/app/enums/index.ts
@@ -1,3 +1,4 @@
 export * from './app-resource-icons';
+export * from './app-themes';
 export * from './storage-keys';
 
