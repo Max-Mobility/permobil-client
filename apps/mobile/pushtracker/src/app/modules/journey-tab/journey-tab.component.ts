a5c14864867c873a4a23c70f6c61dbf56a05b7ba
Brad Martin <bradwaynemartin@gmail.com>
2019-07-09 15:43:03 -0500

    WIP #23

--- apps/mobile/pushtracker/src/app/modules/journey-tab/journey-tab.component.ts
+++ apps/mobile/pushtracker/src/app/modules/journey-tab/journey-tab.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Log } from '@permobil/core';
+import { Page } from 'tns-core-modules/ui/page';
+import { LoggingService } from '../../services';
 
 @Component({
   selector: 'journey',
@@ -7,11 +10,28 @@ import { Log } from '@permobil/core';
   templateUrl: './journey-tab.component.html'
 })
 export class JourneyTabComponent implements OnInit {
-  distancePercentage;
-  coastPercentage;
-  constructor() {}
+  journeyItems;
+  constructor(
+    private _logService: LoggingService,
+    private _translateService: TranslateService,
+    private _page: Page
+  ) {
+    this._page.actionBarHidden = true;
+  }
 
   ngOnInit(): void {
-    Log.D('journey-tab.component ngOnInit');
+    this._logService.logBreadCrumb('journey-tab.component ngOnInit');
+  }
+
+  onJourneyItemTap() {
+    Log.D('journey item tap');
+  }
+
+  onRefreshTap() {
+    Log.D('refresh tap');
+  }
+
+  closeJourneyDetailsLayout() {
+    Log.D('close journey details');
   }
 }
