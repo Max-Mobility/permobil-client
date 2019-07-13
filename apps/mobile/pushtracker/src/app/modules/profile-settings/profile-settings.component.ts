6cca9b98988d507a6d017043bdd93e3ea56af3b1
Gabe Castro <gcastro103@gmail.com>
2019-07-12 19:15:53 -0500

    WIP#25 more changes to settings.

--- apps/mobile/pushtracker/src/app/modules/profile-settings/profile-settings.component.ts
+++ apps/mobile/pushtracker/src/app/modules/profile-settings/profile-settings.component.ts
@@ -3,6 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { Log } from '@permobil/core';
 import { ModalDialogParams } from 'nativescript-angular/modal-dialog';
 import { LoggingService } from '../../services';
+import { EventData } from 'tns-core-modules/data/observable';
 
 @Component({
   selector: 'profile-settings',
@@ -12,12 +13,27 @@ import { LoggingService } from '../../services';
 export class ProfileSettingsComponent implements OnInit {
   private static LOG_TAG = 'profile-settings.component ';
   infoItems;
+  HEIGHT: string;
+  WEIGHT: string;
+  DISTANCE: string;
+  MAX_SPEED: string;
+  ACCELERATION: string;
+  TAP_SENSITIVITY: string;
+  MODE: string;
 
   constructor(
     private _logService: LoggingService,
     private _translateService: TranslateService,
     private _params: ModalDialogParams
-  ) {}
+  ) {
+    this.HEIGHT = 'Feet & inches';
+    this.WEIGHT = 'Pounds';
+    this.DISTANCE = 'Miles';
+    this.MAX_SPEED = '70%';
+    this.ACCELERATION = '70%';
+    this.TAP_SENSITIVITY = '100%';
+    this.MODE = 'MX2+';
+  }
 
   ngOnInit() {
     this._logService.logBreadCrumb(ProfileSettingsComponent.LOG_TAG + `ngOnInit`);
@@ -26,8 +42,36 @@ export class ProfileSettingsComponent implements OnInit {
     );
   }
 
-  onShownModally(args) {
+  onShownModally(args: EventData) {
     Log.D('profile-settings.component modal shown');
+  }
+
+  onHeightTap(args: EventData) {
+    Log.D('height action item tap');
+  }
+
+  onWeightTap(args: EventData) {
+    Log.D('Weight action item tap');
+  }
+
+  onDistanceTap(args: EventData) {
+    Log.D('Distance action item tap');
+  }
+
+  onMaxSpeedTap(args: EventData) {
+    Log.D('Max Speed action item tap');
+  }
+
+  onAccelerationTap(args: EventData) {
+    Log.D('Acceleration action item tap');
+  }
+
+  onTapSensitivityTap(args: EventData) {
+    Log.D('Tap Sensitivity action item tap');
+  }
+
+  onModeTap(args: EventData) {
+    Log.D('Mode action item tap');
   }
 
   closeModal(event) {
