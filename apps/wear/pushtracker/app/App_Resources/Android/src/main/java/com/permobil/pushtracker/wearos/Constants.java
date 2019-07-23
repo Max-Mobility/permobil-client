2ef20bec9cda7783e7391c19ccc8238b7c4eb765
William Emfinger <waemfinger@gmail.com>
2019-07-23 10:38:11 -0500

    udpate package name in constants.

--- apps/wear/pushtracker/app/App_Resources/Android/src/main/java/com/permobil/pushtracker/wearos/Constants.java
+++ apps/wear/pushtracker/app/App_Resources/Android/src/main/java/com/permobil/pushtracker/wearos/Constants.java
@@ -1,7 +1,7 @@
 package com.permobil.pushtracker.wearos;
 
 class Constants {
-    static final String NOTIFICATION_CHANNEL = "com.permobil.psds.wearos.notification_channel";
+    static final String NOTIFICATION_CHANNEL = "com.permobil.pushtracker.wearos.notification_channel";
     static final String ACTION_START_SERVICE = "ACTION_START_SERVICE";
     static final String ACTIVITY_SERVICE_MESSAGE_INTENT_KEY = "ACTIVITY_SERVICE_MESSAGE_INTENT_KEY";
     static final String ACTIVITY_SERVICE_MESSAGE = "ACTIVITY_SERVICE_MESSAGE";
