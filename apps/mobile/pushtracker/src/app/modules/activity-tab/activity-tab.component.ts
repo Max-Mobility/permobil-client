090e7eb3cdd65cee43f470793ce20344ba5defa1
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-08-12 16:37:52 -0500

    WiP activity tab:

--- apps/mobile/pushtracker/src/app/modules/activity-tab/activity-tab.component.ts
+++ apps/mobile/pushtracker/src/app/modules/activity-tab/activity-tab.component.ts
@@ -3,35 +3,70 @@ import { TranslateService } from '@ngx-translate/core';
 import { Log } from '@permobil/core';
 import { ModalDialogParams } from 'nativescript-angular/modal-dialog';
 import { LoggingService } from '../../services';
+import { SelectedIndexChangedEventData } from "tns-core-modules/ui/tab-view";
 
 @Component({
-  selector: 'activity-tab',
-  moduleId: module.id,
-  templateUrl: 'activity-tab.component.html'
+    selector: 'activity-tab',
+    moduleId: module.id,
+    templateUrl: 'activity-tab.component.html'
 })
 export class ActivityTabComponent implements OnInit {
-  private static LOG_TAG = 'activity-tab.component ';
-  infoItems;
+    private static LOG_TAG = 'activity-tab.component ';
+    infoItems;
+    public tabSelectedIndex: number;
+    public displayDay: string;
+    public displayWeek: string;
+    public displayMonth: string;
 
-  constructor(
-    private _logService: LoggingService,
-    private _translateService: TranslateService,
-    private _params: ModalDialogParams
-  ) {}
+    constructor(
+        private _logService: LoggingService,
+        private _translateService: TranslateService,
+        private _params: ModalDialogParams
+    ) {
+        this.tabSelectedIndex = 0;
+        this.displayDay = this._translateService.instant('day');
+        this.displayWeek = this._translateService.instant('week');
+        this.displayMonth = this._translateService.instant('month');
+    }
 
-  ngOnInit() {
-    this._logService.logBreadCrumb(ActivityTabComponent.LOG_TAG + `ngOnInit`);
-    this.infoItems = this._translateService.instant(
-      'activity-tab-component.sections'
-    );
-  }
+    ngOnInit() {
+        this._logService.logBreadCrumb(ActivityTabComponent.LOG_TAG + `ngOnInit`);
+        this.infoItems = this._translateService.instant(
+            'activity-tab-component.sections'
+        );
+    }
 
-  onShownModally(args) {
-    Log.D('activity-tab.component modal shown');
-  }
+    onShownModally(args) {
+        Log.D('activity-tab.component modal shown');
+    }
 
-  closeModal(event) {
-    Log.D('activity-tab.component modal closed');
-    this._params.closeCallback('some value');
-  }
+    closeModal(event) {
+        Log.D('activity-tab.component modal closed');
+        this._params.closeCallback('some value');
+    }
+
+    // displaying the old and new TabView selectedIndex
+    onSelectedIndexChanged(args: SelectedIndexChangedEventData) {
+        if (args.oldIndex !== -1) {
+            const newIndex = args.newIndex;
+            if (newIndex === 0) {
+                // Day
+            } else if (newIndex === 1) {
+                // Week
+            } else if (newIndex === 2) {
+                // Month
+            }
+        }
+    }
+
+    get categoricalSource() {
+        return [
+            { Country: "Germany", Amount: 15, SecondVal: 14, ThirdVal: 24 },
+            { Country: "France", Amount: 13, SecondVal: 23, ThirdVal: 25 },
+            { Country: "Bulgaria", Amount: 24, SecondVal: 17, ThirdVal: 23 },
+            { Country: "Spain", Amount: 11, SecondVal: 19, ThirdVal: 24 },
+            { Country: "USA", Amount: 18, SecondVal: 8, ThirdVal: 21 }
+        ];
+    }
+
 }
