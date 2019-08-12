d3a6121d76f3dc66e168821158d428778e1b54a0
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-08-12 16:38:31 -0500

    tslint fixes

--- apps/mobile/pushtracker/src/app/modules/activity-tab/activity-tab.component.ts
+++ apps/mobile/pushtracker/src/app/modules/activity-tab/activity-tab.component.ts
@@ -59,14 +59,4 @@ export class ActivityTabComponent implements OnInit {
         }
     }
 
-    get categoricalSource() {
-        return [
-            { Country: "Germany", Amount: 15, SecondVal: 14, ThirdVal: 24 },
-            { Country: "France", Amount: 13, SecondVal: 23, ThirdVal: 25 },
-            { Country: "Bulgaria", Amount: 24, SecondVal: 17, ThirdVal: 23 },
-            { Country: "Spain", Amount: 11, SecondVal: 19, ThirdVal: 24 },
-            { Country: "USA", Amount: 18, SecondVal: 8, ThirdVal: 21 }
-        ];
-    }
-
 }
