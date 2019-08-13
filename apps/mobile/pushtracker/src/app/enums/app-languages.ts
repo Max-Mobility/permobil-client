66c48d6f220ab63715058d7138a1778d8ea045c8
Brad Martin <bradwaynemartin@gmail.com>
2019-08-13 17:22:50 -0500

    add language

---
+++ apps/mobile/pushtracker/src/app/enums/app-languages.ts
@@ -0,0 +1,12 @@
+export enum APP_LANGUAGES {
+  'English' = 'en',
+  'Spanish' = 'es',
+  'German' = 'de',
+  'French' = 'fr',
+  'Japanese' = 'ja',
+  'Korean' = 'ko',
+  'Dutch' = 'nl',
+  'Russian' = 'ru',
+  'Swedish' = 'sv',
+  'Chinese (traditional)' = 'zh-CN'
+}
--- apps/mobile/pushtracker/src/app/enums/index.ts
+++ apps/mobile/pushtracker/src/app/enums/index.ts
@@ -1,4 +1,4 @@
+export * from './app-languages';
 export * from './app-resource-icons';
 export * from './app-themes';
 export * from './storage-keys';
-
