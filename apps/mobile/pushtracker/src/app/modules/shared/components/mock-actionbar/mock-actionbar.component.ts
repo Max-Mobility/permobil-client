fae6931b1254a0f9ac97a12733bd26b35bd86109
Brad Martin <bradwaynemartin@gmail.com>
2019-08-16 16:50:23 -0500

    improve mockactionbar layout

--- apps/mobile/pushtracker/src/app/modules/shared/components/mock-actionbar/mock-actionbar.component.ts
+++ apps/mobile/pushtracker/src/app/modules/shared/components/mock-actionbar/mock-actionbar.component.ts
@@ -9,8 +9,9 @@ import { registerElement } from 'nativescript-angular/element-registry';
 import { ModalDialogService } from 'nativescript-angular/modal-dialog';
 import { Toasty } from 'nativescript-toasty';
 import { Color, ContentView } from 'tns-core-modules/ui/content-view';
-import { ProfileSettingsComponent } from '~/app/modules/profile-settings/profile-settings.component';
-import { SupportComponent } from '~/app/modules/support/support.component';
+import { AppInfoComponent } from '../../../../modules/app-info/app-info.component';
+import { ProfileSettingsComponent } from '../../../../modules/profile-settings/profile-settings.component';
+import { SupportComponent } from '../../../../modules/support/support.component';
 
 @Component({
   selector: 'MockActionBar',
@@ -19,19 +20,37 @@ import { SupportComponent } from '~/app/modules/support/support.component';
 })
 export class MockActionbarComponent {
   @Input() title: string;
+  @Input() backNavIcon = 0; // default is the back arrow
   @Input() showBackNav = false;
-  @Input() showSettingsBtn = false;
-  @Input() showSupportBtn = false;
-  @Input() showRefreshBtn = false;
-  @Output() supportTapEvent = new EventEmitter();
   @Output() navBtnEvent = new EventEmitter();
+
+  @Input() showSettingsBtn = false;
   @Output() settingsBtnEvent = new EventEmitter();
+
+  @Input() showSupportBtn = false;
+  @Output() supportTapEvent = new EventEmitter();
+
+  @Input() showRefreshBtn = false;
   @Output() refreshBtnEvent = new EventEmitter();
+
+  @Input() showInfoBtn = false;
+  @Output() infoBtnEvent = new EventEmitter();
+
+  @Input() showMoreBtn = false;
+  @Output() moreBtnEvent = new EventEmitter();
+
+  navIcon; // this sets the font icon in the UI based on the value of backNavIcon
 
   constructor(
     private _modalService: ModalDialogService,
     private _vcRef: ViewContainerRef
-  ) {}
+  ) {
+    if (this.backNavIcon === 0) {
+      this.navIcon = String.fromCharCode(0xe5cd); // arrow
+    } else {
+      this.navIcon = String.fromCharCode(0xe5cd); // close
+    }
+  }
 
   onNavBtnTap() {
     this.navBtnEvent.emit();
@@ -73,8 +92,28 @@ export class MockActionbarComponent {
       });
   }
 
+  onInfoTap() {
+    this._modalService
+      .showModal(AppInfoComponent, {
+        context: {},
+        fullscreen: true,
+        viewContainerRef: this._vcRef
+      })
+      .catch(err => {
+        new Toasty({
+          text:
+            'An unexpected error occurred. If this continues please let us know.',
+          textColor: new Color('#fff000')
+        });
+      });
+  }
+
   onRefreshTap() {
     this.refreshBtnEvent.emit();
+  }
+
+  onMoreTap() {
+    this.moreBtnEvent.emit();
   }
 }
 
