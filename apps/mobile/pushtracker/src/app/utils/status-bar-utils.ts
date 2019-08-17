60303f2e2092cdb06fc40bc902251ca1dec528c7
Brad Martin <bradwaynemartin@gmail.com>
2019-08-16 22:03:27 -0500

    style cleanup to improve themes

--- apps/mobile/pushtracker/src/app/utils/status-bar-utils.ts
+++ apps/mobile/pushtracker/src/app/utils/status-bar-utils.ts
@@ -1,6 +1,5 @@
 /// <reference path="../../../node_modules/tns-platform-declarations/android/android-platform-23.d.ts" />
 
-import { Log } from '@permobil/core';
 import * as TNSApplication from 'tns-core-modules/application';
 import { Color } from 'tns-core-modules/color';
 import { device, isAndroid } from 'tns-core-modules/platform';
@@ -32,7 +31,6 @@ export function clearLightStatusBar() {
     const decorView = window.getDecorView();
 
     const lastFlag = decorView.getSystemUiVisibility(); // get current flags
-    Log.D('LAST UI FLAG', lastFlag);
     // flags = android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR; // use XOR here for remove LIGHT_STATUS_BAR from flags
     if (lastFlag === 8192) decorView.setSystemUiVisibility(0);
   }
@@ -40,7 +38,7 @@ export function clearLightStatusBar() {
 
 export function setDarkStatusBar() {
   if (isAndroid && device.sdkVersion >= '23') {
-    const blackColor = new Color('#000');
+    const blackColor = new Color('#202125');
     const androidActivity: android.app.Activity =
       TNSApplication.android.startActivity ||
       TNSApplication.android.foregroundActivity;
