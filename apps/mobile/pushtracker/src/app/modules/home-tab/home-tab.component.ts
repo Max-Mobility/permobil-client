758371909d954fa13f3d67656fa0583b994f26ac
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-08-20 11:12:13 -0500

    Showing coast time avg and push count in home tab from weekly activity summary

--- apps/mobile/pushtracker/src/app/modules/home-tab/home-tab.component.ts
+++ apps/mobile/pushtracker/src/app/modules/home-tab/home-tab.component.ts
@@ -14,6 +14,8 @@ import { Color } from 'tns-core-modules/color';
 import { LoggingService } from '../../services';
 import { PushTrackerUserService } from '../../services/pushtracker.user.service';
 import { ActivityTabComponent } from '../activity-tab/activity-tab.component';
+import { ActivityService } from '../../services/activity.service';
+import { ObservableArray } from 'tns-core-modules/data/observable-array/observable-array';
 
 @Component({
   selector: 'home-tab',
@@ -35,13 +37,23 @@ export class HomeTabComponent implements OnInit, AfterViewInit {
 
   private routeSub: Subscription; // subscription to route observer
 
+  private _currentDayInView: Date;
+  private _weeklyActivity: ObservableArray<any[]>;
+  private _weeklyActivityCache = {};
+  private _weekStart: Date;
+  private _weekEnd: Date;
+  private _todaysActivity: any;
+  public todayCoastTime: number;
+  public todayPushCount: number;
+
   constructor(
     private _translateService: TranslateService,
     private _logService: LoggingService,
     private _router: Router,
     private _modalService: ModalDialogService,
     private _vcRef: ViewContainerRef,
-    private userService: PushTrackerUserService
+    private userService: PushTrackerUserService,
+    private _activityService: ActivityService
   ) {}
 
   ngOnInit() {
@@ -64,6 +76,12 @@ export class HomeTabComponent implements OnInit, AfterViewInit {
         // this.refreshGoalData();
       }
     });
+    this._currentDayInView = new Date();
+    const sunday = this._getFirstDayOfWeek(this._currentDayInView);
+    this._weekStart = sunday;
+    this._weekEnd = new Date(this._weekStart);
+    this._weekEnd.setDate(this._weekEnd.getDate() + 6);
+    this._loadWeeklyActivity();
   }
 
   onActivityTap() {
@@ -107,5 +125,134 @@ export class HomeTabComponent implements OnInit, AfterViewInit {
     this.distanceData = `2.75`;
 
     this.distanceChartData = null;
+  }
+
+  _getFirstDayOfWeek(date) {
+    date = new Date(date);
+    const day = date.getDay();
+    if (day === 0) return date; // Sunday is the first day of the week
+    const diff = date.getDate() - day;
+    return new Date(date.setDate(diff));
+  }
+
+  async _loadWeeklyActivity() {
+    let didLoad = false;
+    // Check if data is available in daily activity cache first
+    if (!(this._weekStart.toUTCString() in this._weeklyActivityCache)) {
+      didLoad = await this._activityService.loadWeeklyActivity(this._weekStart);
+      if (didLoad) {
+        this._weeklyActivity = new ObservableArray(
+          this._formatActivityForView('Week')
+        );
+        this._weekStart = new Date(this._activityService.weeklyActivity.date);
+        this._weekEnd = new Date(this._weekStart);
+        this._weekEnd.setDate(this._weekEnd.getDate() + 6);
+      } else {
+        this._weeklyActivity = new ObservableArray(
+          this._formatActivityForView('Week')
+        );
+      }
+      // Cache activity by day so we can easily pull it up next time
+      this._weeklyActivityCache[this._weekStart.toUTCString()] = {
+        chartData: this._weeklyActivity,
+        weeklyActivity: this._activityService.weeklyActivity
+      };
+    } else {
+      // We have the data cached. Pull it up
+      didLoad = true;
+      const cache = this._weeklyActivityCache[this._weekStart.toUTCString()];
+      this._weeklyActivity = cache.chartData;
+      this._weekStart = new Date(cache.weeklyActivity.date);
+      this._weekEnd = new Date(this._weekStart);
+      this._weekEnd.setDate(this._weekEnd.getDate() + 6);
+    }
+    const dateFormatted = function(date: Date) {
+      return date.getFullYear() + '/' + ((date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1))
+        + '/' + (date.getDate() < 10 ? '0' + date.getDate() : date.getDate());
+    };
+    const days = this._activityService.weeklyActivity['days'];
+    for (const i in days) {
+      const day = days[i];
+      if (day.date === dateFormatted(this._currentDayInView))
+        this._todaysActivity = day;
+    }
+    this.todayCoastTime = (this._todaysActivity.coast_time_avg || 0).toFixed(1);
+    this.todayPushCount = (this._todaysActivity.push_count || 0).toFixed();
+  }
+
+  _formatActivityForView(viewMode) {
+    if (viewMode === 'Week') {
+      const activity = this._activityService.weeklyActivity;
+      if (activity) {
+        const result = [];
+        const date = new Date(activity.date);
+        const range = function(start, end) {
+          return new Array(end - start + 1)
+            .fill(undefined)
+            .map((_, i) => i + start);
+        };
+        const weekViewDayArray = [];
+        const currentDay = date;
+        let i = 0;
+        while (i < 7) {
+          weekViewDayArray.push(new Date(currentDay));
+          currentDay.setDate(currentDay.getDate() + 1);
+          i = i + 1;
+        }
+        const days = activity.days;
+        let j = 0;
+        const dayNames: string[] = [
+          'Sun',
+          'Mon',
+          'Tue',
+          'Wed',
+          'Thu',
+          'Fri',
+          'Sat'
+        ];
+        for (const i in weekViewDayArray) {
+          const dayInWeek = weekViewDayArray[i];
+          if (days && j < days.length) {
+            while (j < days.length) {
+              const dailyActivity = days[j];
+              if (
+                dayInWeek.toDateString() ===
+                new Date(dailyActivity.date).toDateString()
+              ) {
+                // We have daily activity for this day
+                result.push({
+                  xAxis: dayNames[parseInt(i)],
+                  coastTime: dailyActivity.coast_time_avg || 0,
+                  pushCount: dailyActivity.push_count || 0,
+                  date: dayInWeek
+                });
+                j += 1;
+                continue;
+              } else {
+                result.push({
+                  xAxis: dayNames[parseInt(i)],
+                  coastTime: 0,
+                  pushCount: 0,
+                  date: dayInWeek
+                });
+                break;
+              }
+            }
+          } else {
+            result.push({
+              xAxis: dayNames[parseInt(i)],
+              coastTime: 0,
+              pushCount: 0,
+              date: dayInWeek
+            });
+          }
+        }
+        result.unshift({ xAxis: ' ', coastTime: 0, pushCount: 0 });
+        result.unshift({ xAxis: '  ', coastTime: 0, pushCount: 0 });
+        result.push({ xAxis: '       ', coastTime: 0, pushCount: 0 });
+        result.push({ xAxis: '        ', coastTime: 0, pushCount: 0 });
+        return result;
+      }
+    }
   }
 }
