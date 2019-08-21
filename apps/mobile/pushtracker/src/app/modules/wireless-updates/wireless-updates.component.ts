aa0756ca3978e35ebd15297bff946033286d13aa
Brad Martin <bradwaynemartin@gmail.com>
2019-08-21 16:14:28 -0500

    fix aot

--- apps/mobile/pushtracker/src/app/modules/wireless-updates/wireless-updates.component.ts
+++ apps/mobile/pushtracker/src/app/modules/wireless-updates/wireless-updates.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Log } from '@permobil/core';
 import { ModalDialogParams } from 'nativescript-angular/modal-dialog';
 import { LoggingService } from '../../services';
 
@@ -9,6 +10,9 @@ import { LoggingService } from '../../services';
   templateUrl: 'wireless-updates.component.html'
 })
 export class WirelessUpdatesComponent implements OnInit {
+  ptCirclePercentage: number = 83;
+  sdCirclePercentage: number = 30;
+
   constructor(
     private _logService: LoggingService,
     private _translateService: TranslateService,
@@ -25,5 +29,13 @@ export class WirelessUpdatesComponent implements OnInit {
 
   closeModal() {
     this._params.closeCallback();
+  }
+
+  onStartTap(device: string) {
+    Log.D('start tap');
+  }
+
+  onStopTap(device: string) {
+    Log.D('stop tap');
   }
 }
