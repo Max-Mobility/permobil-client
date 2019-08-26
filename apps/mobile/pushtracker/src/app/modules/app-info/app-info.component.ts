63a425b66847a7ec18799d4e8364be9aeed0c7ef
Brad Martin <bradwaynemartin@gmail.com>
2019-08-26 16:46:54 -0500

    clean up app-info

--- apps/mobile/pushtracker/src/app/modules/app-info/app-info.component.ts
+++ apps/mobile/pushtracker/src/app/modules/app-info/app-info.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ModalDialogParams } from 'nativescript-angular/modal-dialog';
-import * as appSettings from 'tns-core-modules/application-settings';
-import { AppResourceIcons, APP_THEMES, STORAGE_KEYS } from '../../enums';
 import { LoggingService } from '../../services';
 
 @Component({
@@ -12,23 +10,12 @@ import { LoggingService } from '../../services';
 })
 export class AppInfoComponent implements OnInit {
   infoItems;
-  androidBackIcon;
 
   constructor(
     private _logService: LoggingService,
     private _translateService: TranslateService,
     private _params: ModalDialogParams
-  ) {
-    const currentTheme = appSettings.getString(
-      STORAGE_KEYS.APP_THEME,
-      APP_THEMES.DEFAULT
-    );
-
-    this.androidBackIcon =
-      currentTheme === APP_THEMES.DEFAULT
-        ? AppResourceIcons.BLACK_BACK_NAV
-        : AppResourceIcons.WHITE_BACK_NAV;
-  }
+  ) {}
 
   ngOnInit() {
     this._logService.logBreadCrumb('app-info.component OnInit');
