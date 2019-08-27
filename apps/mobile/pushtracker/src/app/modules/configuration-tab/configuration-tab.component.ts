6574cc5e1baf11603dc9f14222a2e0732dd9ab66
Brad Martin <bradwaynemartin@gmail.com>
2019-08-27 12:08:30 -0700

    clean up

--- apps/mobile/pushtracker/src/app/modules/configuration-tab/configuration-tab.component.ts
+++ apps/mobile/pushtracker/src/app/modules/configuration-tab/configuration-tab.component.ts
@@ -1,38 +1,43 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { PushTrackerUserService } from '../../services';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { PushTrackerUser } from '@permobil/core';
 import { User as KinveyUser } from 'kinvey-nativescript-sdk';
-import { Page } from 'ui/page';
+import { Page } from 'tns-core-modules/ui/page';
+import { PushTrackerUserService } from '../../services';
 
 @Component({
-    selector: 'configuration-tab',
-    moduleId: module.id,
-    templateUrl: './configuration-tab.component.html'
+  selector: 'configuration-tab',
+  moduleId: module.id,
+  templateUrl: './configuration-tab.component.html'
 })
 export class ConfigurationTabComponent implements OnInit {
-    private _user: PushTrackerUser;
+  private _user: PushTrackerUser;
 
-    constructor(
-        private _translateService: TranslateService,
-        private _router: Router,
-        private userService: PushTrackerUserService,
-        private page: Page
-    ) {
-        page.actionBarHidden = true;
-    }
+  constructor(
+    private _translateService: TranslateService,
+    private _router: Router,
+    private userService: PushTrackerUserService,
+    private _page: Page
+  ) {
+    this._page.actionBarHidden = true;
+  }
 
-    ngOnInit() {
-        this.userService.user.subscribe(user => {
-            this._user = user;
-        });
-    }
+  ngOnInit() {
+    this.userService.user.subscribe(user => {
+      this._user = user;
+    });
+  }
 
-    onConfigurationSelection(event, selection) {
-        this._user.data.control_configuration = selection;
-        this.userService.updateDataProperty('control_configuration', this._user.data.control_configuration);
-        KinveyUser.update({ control_configuration: this._user.data.control_configuration });
-        this._router.navigate(['/tabs/default']);
-    }
+  onConfigurationSelection(event, selection) {
+    this._user.data.control_configuration = selection;
+    this.userService.updateDataProperty(
+      'control_configuration',
+      this._user.data.control_configuration
+    );
+    KinveyUser.update({
+      control_configuration: this._user.data.control_configuration
+    });
+    this._router.navigate(['/tabs/default']);
+  }
 }
