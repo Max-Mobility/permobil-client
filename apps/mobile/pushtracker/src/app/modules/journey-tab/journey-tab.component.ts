6196e47fc6a342745e94e9afc6bd9a5f2eaeaeff
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-09-09 11:22:11 -0500

    Added ashy color to home tab circle and updated journey merge logic

--- apps/mobile/pushtracker/src/app/modules/journey-tab/journey-tab.component.ts
+++ apps/mobile/pushtracker/src/app/modules/journey-tab/journey-tab.component.ts
@@ -478,7 +478,7 @@ export class JourneyTabComponent implements OnInit {
       });
     };
 
-    const HALF_HOUR = 30 * 60 * 1000; /* ms */
+    const FORTY_FIVE_MINUTES = 45 * 60 * 1000; /* ms */
 
     if (journeyList.length > 1) {
       for (const i in journeyList) {
@@ -497,7 +497,7 @@ export class JourneyTabComponent implements OnInit {
           const timeDiff = secondDate.getTime() - firstDate.getTime();
           if (first.stats.journeyType === second.stats.journeyType &&
               first.stats.timeOfDay === second.stats.timeOfDay &&
-              timeDiff === HALF_HOUR) {
+              timeDiff < FORTY_FIVE_MINUTES) {
             journeyList[firstIndex].stats.coastTime =
               ((journeyList[firstIndex].stats.coastTimeTotal || 0) + second.stats.coastTimeTotal || 0) /
               (((journeyList[firstIndex].stats.pushCount || 0) + second.stats.pushCount || 0) || 1);
