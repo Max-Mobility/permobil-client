57d00d775e863d5b2bbb439d4d7745bb439fd870
Brad Martin <bradwaynemartin@gmail.com>
2019-09-10 13:21:31 -0500

    fxi tslint

--- @permobil/nativescript/src/plugins/nativescript-wear-os-comms/demo/app/home/home-view-model.ts
+++ @permobil/nativescript/src/plugins/nativescript-wear-os-comms/demo/app/home/home-view-model.ts
@@ -1,7 +1,7 @@
-import { Observable } from "tns-core-modules/data/observable";
+import { Observable } from 'tns-core-modules/data/observable';
 
 export class HomeViewModel extends Observable {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 }
