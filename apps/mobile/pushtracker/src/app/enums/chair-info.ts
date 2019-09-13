fb61d02ca22ca001fd620a4990ca30658c634fab
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-09-13 16:03:09 -0500

    Chair info enums #297

---
+++ apps/mobile/pushtracker/src/app/enums/chair-info.ts
@@ -0,0 +1,18 @@
+export enum CHAIR_TYPE {
+    'RIGID' = 'Rigid',
+    'FOLDING' = 'Folding',
+    'PEDIATRIC' = 'Pediatric'
+}
+
+export enum CHAIR_MAKE {
+    'COLOURS' = 'Colours',
+    'INVACARE_KUSCHALL' = 'Invacare / Küschall',
+    'KARMAN' = 'Karman',
+    'KI' = 'Ki',
+    'MOTION_COMPOSITES' = 'Motion Composites',
+    'PANTHERA' = 'Panthera',
+    'QUICKIE_SOPUR_RGK' = 'Quickie / Sopur / RGK',
+    'TILITE' = 'TiLite',
+    'TOP_END' = 'Top End',
+    'OTHER' = 'Other'
+}
