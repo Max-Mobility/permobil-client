395be7ff23f709579686fe0a0143634c5ccfc096
William Emfinger <waemfinger@gmail.com>
2019-09-24 13:45:50 -0500

    closes #336 make sure to use separate key for units communication to complications.

--- apps/wear/smartdrive/app/App_Resources/Android/src/main/java/com/permobil/smartdrive/wearos/ComplicationToggleReceiver.java
+++ apps/wear/smartdrive/app/App_Resources/Android/src/main/java/com/permobil/smartdrive/wearos/ComplicationToggleReceiver.java
@@ -73,7 +73,8 @@ public class ComplicationToggleReceiver extends BroadcastReceiver {
     editor.putFloat(preferenceKey, appSharedPreferences.getFloat(dataId, 0.0f));
 
     if (unitsId != null) {
-      editor.putString(preferenceKey, appSharedPreferences.getString(unitsId, "english"));
+      String unitsKey = getPreferenceKey(provider, complicationId, unitsId);
+      editor.putString(unitsKey, appSharedPreferences.getString(unitsId, "english"));
     }
 
     // now actually tell the editor to apply the changes
