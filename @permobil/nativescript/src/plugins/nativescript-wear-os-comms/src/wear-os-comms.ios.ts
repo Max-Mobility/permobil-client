ee30dcc7957aba6547f1adb054c566a75bf1d678
William Emfinger <waemfinger@gmail.com>
2019-09-24 16:22:01 -0500

    WIP #282 #253 fleshing out more of the iOS side of the wear-os-comms library for finding devices and sending data.

--- @permobil/nativescript/src/plugins/nativescript-wear-os-comms/src/wear-os-comms.ios.ts
+++ @permobil/nativescript/src/plugins/nativescript-wear-os-comms/src/wear-os-comms.ios.ts
@@ -3,16 +3,87 @@ import { Common } from './wear-os-comms.common';
 
 export class WearOsComms extends Common {
   private static _bluetooth: Bluetooth = new Bluetooth();
+
+  // device address
+  private static pairedCompanion: string = null;
+
+  private static ServiceUUID: string = "";
+
   constructor() {
     super();
   }
 
-  public static sendMessage(channel: string, msg: string) {
-    return new Promise((resolve, reject) => {
+  public static saveCompanion(address: string) {
+    if (address !== null) {
+      WearOsComms.pairedCompanion = address;
+    }
+  }
+
+  public static hasCompanion() {
+    return WearOsComms.pairedCompanion !== null;
+  }
+
+  public static async findAvailableCompanions(timeoutSeconds: number) {
+    // scan for service and pass the resolve function argument to the
+    // onCompanionDiscoveredCallback
+    const promise = await new Promise(async (resolve, reject) => {
       try {
-        // this._bluetooth.enable();
-        // TODO: figure out how the bluetooth message send will work here
-        resolve();
+        await WearOsComms._bluetooth.startScanning({
+          serviceUUIDs: [ WearOsComms.ServiceUUID ],
+          seconds: timeoutSeconds,
+          onDiscovered: (peripheral: any) => {
+            WearOsComms._bluetooth.stopScanning();
+            resolve(peripheral.UUID);
+          },
+          skipPermissionCheck: false
+        });
+        // resolve here if we've gotten through scanning and have not
+        // found any devices through the callback
+        resolve(null);
+      } catch (err) {
+        console.error('findAvailableCompanions error:', err);
+        // resolve with no devices found
+        resolve(null);
+      }
+    });
+  }
+
+  private static async getCompanion() {
+    let companion = null;
+    if (WearOsComms.hasCompanion()) {
+      companion = WearOsComms.pairedCompanion;
+    } else {
+      // scan for the right services
+      const address = await WearOsComms.findAvailableCompanions(1);
+      if (address && address.length) {
+        companion = address;
+      }
+    }
+    return companion;
+  }
+
+  public static async sendMessage(channel: string, msg: string) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        // state variables
+        let didSend = false;
+        // get the companion
+        const companion = await WearOsComms.getCompanion();
+        // connect to the companion
+        WearOsComms._bluetooth.connect({
+          UUID: companion,
+          onConnected: (peripheral: any) => {
+            // TODO: send the data to the companion
+            // TODO: disconnect from the companion
+          },
+          onDisconnected: (peripheral: any) => {
+            if (didSend) {
+              resolve();
+            } else {
+              reject('failed to send');
+            }
+          }
+        });
       } catch (error) {
         reject(error);
       }
