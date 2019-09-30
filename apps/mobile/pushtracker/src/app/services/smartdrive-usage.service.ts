f8997045d5aab3dafd472c3f7aa8c92750fd9596
William Emfinger <waemfinger@gmail.com>
2019-09-30 14:11:21 -0500

    closes #377 fix querying the database after datastore type change.

--- apps/mobile/pushtracker/src/app/services/smartdrive-usage.service.ts
+++ apps/mobile/pushtracker/src/app/services/smartdrive-usage.service.ts
@@ -32,28 +32,28 @@ export class SmartDriveUsageService {
 
       // Run a .find first to get the _id of the daily activity
       {
-        const stream = this.datastore.find(query);
-        return stream.toPromise().then(data => {
-          if (data && data.length) {
-            const id = data[0]._id;
-            dailyUsage._id = id;
-            dailyUsage.distance_smartdrive_drive_start = data[0].distance_smartdrive_drive_start;
-            dailyUsage.distance_smartdrive_coast_start = data[0].distance_smartdrive_coast_start;
-          }
-          else {
-            // First record for this day
-            // Save distance_start
-            dailyUsage.distance_smartdrive_drive_start = dailyUsage.distance_smartdrive_drive;
-            dailyUsage.distance_smartdrive_coast_start = dailyUsage.distance_smartdrive_coast;
-          }
-          return this.datastore.save(dailyUsage)
-            .then((_) => {
-              return true;
-            }).catch((error) => {
-              this._logService.logException(error);
-              return false;
-            });
-        });
+        return this.datastore.find(query)
+          .then(data => {
+            if (data && data.length) {
+              const id = data[0]._id;
+              dailyUsage._id = id;
+              dailyUsage.distance_smartdrive_drive_start = data[0].distance_smartdrive_drive_start;
+              dailyUsage.distance_smartdrive_coast_start = data[0].distance_smartdrive_coast_start;
+            }
+            else {
+              // First record for this day
+              // Save distance_start
+              dailyUsage.distance_smartdrive_drive_start = dailyUsage.distance_smartdrive_drive;
+              dailyUsage.distance_smartdrive_coast_start = dailyUsage.distance_smartdrive_coast;
+            }
+            return this.datastore.save(dailyUsage)
+              .then((_) => {
+                return true;
+              }).catch((error) => {
+                this._logService.logException(error);
+                return false;
+              });
+          });
       }
 
     } catch (err) {
