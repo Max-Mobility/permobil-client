46c712419984957f9c3bfc301a7ad4a891c1255a
Brad Martin <bradwaynemartin@gmail.com>
2019-10-04 13:03:52 -0500

    move javaproxy files out and cleanup main-view-model

---
+++ apps/wear/pushtracker/app/data-broadcast-receiver.ts
@@ -0,0 +1,14 @@
+@JavaProxy('com.permobil.pushtracker.DataBroadcastReceiver')
+export class DataBroadcastReceiver extends android.content.BroadcastReceiver {
+  public onReceiveFunction: any = null;
+  constructor() {
+    super();
+    return global.__native(this);
+  }
+  onReceive(
+    androidContext: android.content.Context,
+    intent: android.content.Intent
+  ) {
+    if (this.onReceiveFunction) this.onReceiveFunction(androidContext, intent);
+  }
+}
