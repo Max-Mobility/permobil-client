7ad62cea1333782af856fe6aa8de9425d9477a31
William Emfinger <waemfinger@gmail.com>
2019-10-09 13:55:57 -0500

    some comments.

--- @permobil/nativescript/src/plugins/nativescript-wear-os-comms/src/wear-os-comms.common.ts
+++ @permobil/nativescript/src/plugins/nativescript-wear-os-comms/src/wear-os-comms.common.ts
@@ -1,11 +1,15 @@
 export class Common {
+  // bluetooth services / characteristics for wearos <-> ios
+  // communications
   public static ServiceUUID: string = 'a04cfcb6-5dc5-4a27-bd04-e2dd0cb1ff44';
   public static MessageCharacteristicUUID: string =
     'f733bcab-767f-4b75-8aea-33e6a56fc87a';
   public static DataCharacteristicUUID: string =
     'f733bcab-767f-4b75-8aea-33e6a56fc88a';
 
+  // for saving the companion to app settings
   public static APP_SETTINGS_COMPANION_KEY: string = 'wear-os-comms.companion';
 
+  // messages are defined by ${path}${delimeter}${message}
   public static MessageDelimeter: string = '::::::';
 }
