4436c6c6459a089622f3267300619715408f2335
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-10-15 17:21:28 -0500

    tslint fixes

--- apps/wear/smartdrive/app/pages/main-page/main-view-model.ts
+++ apps/wear/smartdrive/app/pages/main-page/main-view-model.ts
@@ -125,7 +125,7 @@ export class MainViewModel extends Observable {
   @Prop() activeSettingToChange = '';
   @Prop() changeSettingKeyString = ' ';
   @Prop() changeSettingKeyValue: any = ' ';
-  
+
   /**
    *
    * SmartDrive Related Data
