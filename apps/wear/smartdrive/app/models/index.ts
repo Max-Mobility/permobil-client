47568663ed3a2cdc5fa1dd66e07c0482e648fe27
Brad Martin <bradwaynemartin@gmail.com>
2019-10-28 13:11:29 -0500

    move smartdriveexception class SD.W

--- apps/wear/smartdrive/app/models/index.ts
+++ apps/wear/smartdrive/app/models/index.ts
@@ -1,3 +1,5 @@
 export * from './device-base';
 export * from './smartdrive';
+export * from './smartdrive-exception';
 export * from './tap-detector';
+
---
+++ apps/wear/smartdrive/app/models/smartdrive-exception.ts
@@ -0,0 +1,6 @@
+export class SmartDriveException extends Error {
+    constructor(...args) {
+      super(...args);
+      this.name = 'SmartDriveMX2+ Exception';
+    }
+  }
