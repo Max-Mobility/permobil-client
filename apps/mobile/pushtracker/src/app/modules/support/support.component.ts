c60c3b077a14522719f961d39e37b2499fc31b88
William Emfinger <waemfinger@gmail.com>
2019-11-10 15:03:44 -0600

    added search bar to support page.

--- apps/mobile/pushtracker/src/app/modules/support/support.component.ts
+++ apps/mobile/pushtracker/src/app/modules/support/support.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalDialogParams } from '@nativescript/angular';
 import { isIOS } from '@nativescript/core';
+import { SearchBar } from '@nativescript/core/ui';
 import { TranslateService } from '@ngx-translate/core';
 import { LoggingService } from '../../services';
 
@@ -11,17 +12,27 @@ import { LoggingService } from '../../services';
 })
 export class SupportComponent implements OnInit {
   supportItems;
+  searchPhrase: string = '';
+
+  private _allSupportItems;
+
   constructor(
     private _logService: LoggingService,
     private _translateService: TranslateService,
     private _params: ModalDialogParams
-  ) {}
+  ) { }
 
   ngOnInit() {
     this._logService.logBreadCrumb(SupportComponent.name, 'OnInit');
     this.supportItems = this._translateService.instant(
       'support-component.faqs'
     );
+    this._allSupportItems = this.supportItems.map(i => {
+      return {
+        a: i.a,
+        q: i.q
+      };
+    });
   }
 
   onItemLoading(args) {
@@ -33,5 +44,23 @@ export class SupportComponent implements OnInit {
 
   onNavBtnTap() {
     this._params.closeCallback('');
+  }
+
+  onSubmit(args) {
+    const searchBar = args.object as SearchBar;
+    this.searchPhrase = searchBar.text;
+    const relevant = this._allSupportItems.filter(i => {
+      return i.a.includes(this.searchPhrase) || i.q.includes(this.searchPhrase);
+    });
+    this.supportItems.splice(0, this.supportItems.length, ...relevant);
+  }
+
+  onTextChanged(args) {
+    const searchBar = args.object as SearchBar;
+  }
+
+  onClear(args) {
+    const searchBar = args.object as SearchBar;
+    this.supportItems.splice(0, this.supportItems.length, ...this._allSupportItems);
   }
 }
