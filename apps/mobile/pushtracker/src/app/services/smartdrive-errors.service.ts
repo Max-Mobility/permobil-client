438569298af57cd474ae79a0d95028bda6dd39e4
William Emfinger <waemfinger@gmail.com>
2019-11-13 14:54:20 -0600

    closes #512 closes #511 closes #509 bugfix in smartdrive usage and error info sending to backend

--- apps/mobile/pushtracker/src/app/services/smartdrive-errors.service.ts
+++ apps/mobile/pushtracker/src/app/services/smartdrive-errors.service.ts
@@ -32,27 +32,29 @@ export class SmartDriveErrorsService {
 
       // Run a .find first to get the _id of the daily activity
       {
-        const stream = this.datastore.find(query);
-        return stream.toPromise().then(data => {
-          if (data && data.length) {
-            const id = data[0]._id;
-            dailyErrors._id = id;
-            // Accumulate errors w/ saved info in DB
-            dailyErrors.num_battery_voltage_errors += data[0].num_battery_voltage_errors || 0;
-            dailyErrors.num_over_current_errors += data[0].num_over_current_errors || 0;
-            dailyErrors.num_motor_phase_errors += data[0].num_motor_phase_errors || 0;
-            dailyErrors.num_gyro_range_errors += data[0].num_gyro_range_errors || 0;
-            dailyErrors.num_over_temperature_errors += data[0].num_over_temperature_errors || 0;
-            dailyErrors.num_ble_disconnect_errors += data[0].num_ble_disconnect_errors || 0;
-          }
-          return this.datastore.save(dailyErrors)
-            .then((_) => {
-              return true;
-            }).catch((error) => {
-              this._logService.logException(error);
-              return false;
-            });
-        });
+        const stream = this.datastore
+          .find(query)
+          .then((data: any[]) => {
+            if (data && data.length) {
+              const id = data[0]._id;
+              dailyErrors._id = id;
+              // Accumulate errors w/ saved info in DB
+              dailyErrors.num_battery_voltage_errors += data[0].num_battery_voltage_errors || 0;
+              dailyErrors.num_over_current_errors += data[0].num_over_current_errors || 0;
+              dailyErrors.num_motor_phase_errors += data[0].num_motor_phase_errors || 0;
+              dailyErrors.num_gyro_range_errors += data[0].num_gyro_range_errors || 0;
+              dailyErrors.num_over_temperature_errors += data[0].num_over_temperature_errors || 0;
+              dailyErrors.num_ble_disconnect_errors += data[0].num_ble_disconnect_errors || 0;
+            }
+            return this.datastore.save(dailyErrors);
+          })
+          .then((_) => {
+            return true;
+          })
+          .catch((error) => {
+            this._logService.logException(error);
+            return false;
+          });
       }
 
     } catch (err) {
