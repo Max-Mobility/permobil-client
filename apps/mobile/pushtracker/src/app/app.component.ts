ddf819987ce87b7339997033cb9c24ecbb7880be
William Emfinger <waemfinger@gmail.com>
2019-11-15 15:33:55 -0600

    closes #527 split the device language by "-" to ensure we only get the major language for setting the language to use in the translate service.

--- apps/mobile/pushtracker/src/app/app.component.ts
+++ apps/mobile/pushtracker/src/app/app.component.ts
@@ -49,7 +49,7 @@ export class AppComponent implements OnInit {
     // *** The value being set must match a translation .json file in assets/i18n/ or it will fail ***
     // wrapping this in try/catch due to https://github.com/PushTracker/EvalApp/issues/43
     try {
-      const defaultLanguage = device.language;
+      const defaultLanguage = device.language.split('-')[0];
       this._logService.logBreadCrumb(
         AppComponent.name,
         'Setting default language to ' + defaultLanguage
