7919ba3980b89609843fa60c1b6d9ec8bf0d6cd2
Pranav Srinivas Kumar <pranav@Pranavs-MacBook-Pro.local>
2019-11-18 08:11:58 -0600

    Commented out some log exceptions

--- apps/mobile/pushtracker/src/app/services/translation.service.ts
+++ apps/mobile/pushtracker/src/app/services/translation.service.ts
@@ -65,7 +65,11 @@ export class TranslationService extends Observable {
             const dl = await TranslationService.download(f);
             files.push(dl);
           } catch (err) {
-            this._logService.logException(err);
+            this._logService.logBreadCrumb(
+              TranslationService.name,
+              'Could not download language files'
+            );
+            // this._logService.logException(err);
           }
         }
       }
@@ -154,7 +158,7 @@ export class TranslationService extends Observable {
         TranslationService.name,
         'Could not save language file: ' + err
       );
-      this._logService.logException(err);
+      // this._logService.logException(err);
     }
   }
 
