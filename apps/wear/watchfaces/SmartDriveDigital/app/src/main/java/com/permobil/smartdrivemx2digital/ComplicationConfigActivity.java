05f540cafee4257fd9bdeae9b13ecb15a51caf29
Brad Martin <bradwaynemartin@gmail.com>
2019-11-21 09:57:27 -0600

    clean up complications to support top center

--- apps/wear/watchfaces/SmartDriveDigital/app/src/main/java/com/permobil/smartdrivemx2digital/ComplicationConfigActivity.java
+++ apps/wear/watchfaces/SmartDriveDigital/app/src/main/java/com/permobil/smartdrivemx2digital/ComplicationConfigActivity.java
@@ -23,7 +23,6 @@ import java.util.concurrent.Executors;
  * the left and right complications of watch face.
  */
 public class ComplicationConfigActivity extends Activity implements View.OnClickListener {
-
     private static final String TAG = "ConfigActivity";
 
     static final int COMPLICATION_CONFIG_REQUEST_CODE = 1001;
@@ -33,116 +32,63 @@ public class ComplicationConfigActivity extends Activity implements View.OnClick
      * configuration Activity know which complication locations are supported, their ids, and
      * supported complication data types.
      */
-
     public enum ComplicationLocation {
-        LEFT,
-        BACKGROUND,
         CENTER
     }
 
     private int mLeftComplicationId;
     private int mBackgroundComplicationId;
     private int mCenterComplicationId;
-
     // Selected complication id by user.
     private int mSelectedComplicationId;
-
     // ComponentName used to identify a specific service that renders the watch face.
     private ComponentName mWatchFaceComponentName;
-
     // Required to retrieve complication data from watch face for preview.
     private ProviderInfoRetriever mProviderInfoRetriever;
-
-    private ImageView mLeftComplicationBackground;
-    private ImageView mBackgroundComplicationBackground;
     private ImageView mCenterComplicationBackground;
-
-    private ImageButton mLeftComplication;
-    private ImageButton mBackgroundComplication;
     private ImageButton mCenterComplication;
-
     private Drawable mDefaultAddComplicationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_config);
 
         mDefaultAddComplicationDrawable = getDrawable(R.drawable.add_complication);
-
-
         mSelectedComplicationId = -1;
-
-        mLeftComplicationId =
-                ComplicationWatchFaceService.getComplicationId(ComplicationLocation.LEFT);
-        mBackgroundComplicationId =
-                ComplicationWatchFaceService.getComplicationId(ComplicationLocation.BACKGROUND);
-        mCenterComplicationId =
-                ComplicationWatchFaceService.getComplicationId(ComplicationLocation.CENTER);
-
-        mWatchFaceComponentName =
-                new ComponentName(getApplicationContext(), ComplicationWatchFaceService.class);
-
-        // Sets up left complication preview.
-        mLeftComplicationBackground = findViewById(R.id.left_complication_background);
-        mLeftComplication = findViewById(R.id.left_complication);
-        mLeftComplication.setOnClickListener(this);
-
-        // Sets default as "Add Complication" icon.
-        mLeftComplication.setImageDrawable(mDefaultAddComplicationDrawable);
-        mLeftComplicationBackground.setVisibility(View.INVISIBLE);
-
-        // Sets up right complication preview.
-        mBackgroundComplicationBackground = findViewById(R.id.background_complication_background);
-        mBackgroundComplication = findViewById(R.id.background_complication);
-        mBackgroundComplication.setOnClickListener(this);
-
-        // Sets default as "Add Complication" icon.
-        mBackgroundComplication.setImageDrawable(mDefaultAddComplicationDrawable);
-        mBackgroundComplicationBackground.setVisibility(View.INVISIBLE);
+        mCenterComplicationId = ComplicationWatchFaceService.getComplicationId(ComplicationLocation.CENTER);
+        mWatchFaceComponentName = new ComponentName(getApplicationContext(), ComplicationWatchFaceService.class);
 
         // Sets up center complication preview.
         mCenterComplicationBackground = findViewById(R.id.center_complication_background);
         mCenterComplication = findViewById(R.id.center_complication);
         mCenterComplication.setOnClickListener(this);
-
         // Sets default as "Add Complication" icon.
         mCenterComplication.setImageDrawable(mDefaultAddComplicationDrawable);
         mCenterComplicationBackground.setVisibility(View.INVISIBLE);
 
-
         // Initialization of code to retrieve active complication data for the watch face.
-        mProviderInfoRetriever =
-                new ProviderInfoRetriever(getApplicationContext(), Executors.newCachedThreadPool());
+        mProviderInfoRetriever = new ProviderInfoRetriever(getApplicationContext(), Executors.newCachedThreadPool());
         mProviderInfoRetriever.init();
-
         retrieveInitialComplicationsData();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-
         // Required to release retriever for active complication data.
         mProviderInfoRetriever.release();
     }
 
 
     public void retrieveInitialComplicationsData() {
-
         final int[] complicationIds = ComplicationWatchFaceService.getComplicationIds();
 
         mProviderInfoRetriever.retrieveProviderInfo(
                 new ProviderInfoRetriever.OnProviderInfoReceivedCallback() {
                     @Override
-                    public void onProviderInfoReceived(
-                            int watchFaceComplicationId,
-                            @Nullable ComplicationProviderInfo complicationProviderInfo) {
-
+                    public void onProviderInfoReceived(int watchFaceComplicationId, @Nullable ComplicationProviderInfo complicationProviderInfo) {
                         Log.d(TAG, "onProviderInfoReceived: " + complicationProviderInfo);
-
                         updateComplicationViews(watchFaceComplicationId, complicationProviderInfo);
                     }
                 },
@@ -155,13 +101,6 @@ public class ComplicationConfigActivity extends Activity implements View.OnClick
         if (view.equals(mCenterComplication)) {
             Log.d(TAG, "Center Complication click()");
             launchComplicationHelperActivity(ComplicationLocation.CENTER);
-
-        } else if (view.equals(mBackgroundComplication)) {
-            Log.d(TAG, "Background Complication click()");
-            launchComplicationHelperActivity(ComplicationLocation.BACKGROUND);
-        } else if (view.equals(mLeftComplication)) {
-            Log.d(TAG, "Left Complication click()");
-            launchComplicationHelperActivity(ComplicationLocation.LEFT);
         }
     }
 
@@ -169,58 +108,31 @@ public class ComplicationConfigActivity extends Activity implements View.OnClick
     // class, so user can choose their complication data provider.
 
     private void launchComplicationHelperActivity(ComplicationLocation complicationLocation) {
-
-        mSelectedComplicationId =
-                ComplicationWatchFaceService.getComplicationId(complicationLocation);
+        mSelectedComplicationId = ComplicationWatchFaceService.getComplicationId(complicationLocation);
 
         if (mSelectedComplicationId >= 0) {
+            int[] supportedTypes = ComplicationWatchFaceService.getSupportedComplicationTypes(complicationLocation);
 
-            int[] supportedTypes =
-                    ComplicationWatchFaceService.getSupportedComplicationTypes(
-                            complicationLocation);
-
-            startActivityForResult(
-                    ComplicationHelperActivity.createProviderChooserHelperIntent(
-                            getApplicationContext(),
-                            mWatchFaceComponentName,
-                            mSelectedComplicationId,
-                            supportedTypes),
-                    ComplicationConfigActivity.COMPLICATION_CONFIG_REQUEST_CODE);
-
+            startActivityForResult(ComplicationHelperActivity.createProviderChooserHelperIntent(
+                    getApplicationContext(),
+                    mWatchFaceComponentName,
+                    mSelectedComplicationId,
+                    supportedTypes),
+                    ComplicationConfigActivity.COMPLICATION_CONFIG_REQUEST_CODE
+            );
         } else {
             Log.d(TAG, "Complication not supported by watch face.");
         }
     }
 
-    public void updateComplicationViews(
-            int watchFaceComplicationId, ComplicationProviderInfo complicationProviderInfo) {
+    public void updateComplicationViews(int watchFaceComplicationId, ComplicationProviderInfo complicationProviderInfo) {
         Log.d(TAG, "updateComplicationViews(): id: " + watchFaceComplicationId);
         Log.d(TAG, "\tinfo: " + complicationProviderInfo);
 
-        if (watchFaceComplicationId == mLeftComplicationId) {
-            if (complicationProviderInfo != null) {
-                mLeftComplication.setImageIcon(complicationProviderInfo.providerIcon);
-                mLeftComplicationBackground.setVisibility(View.VISIBLE);
-
-            } else {
-                mLeftComplication.setImageDrawable(mDefaultAddComplicationDrawable);
-                mLeftComplicationBackground.setVisibility(View.INVISIBLE);
-            }
-
-        } else if (watchFaceComplicationId == mBackgroundComplicationId) {
-            if (complicationProviderInfo != null) {
-                mBackgroundComplication.setImageIcon(complicationProviderInfo.providerIcon);
-                mBackgroundComplicationBackground.setVisibility(View.VISIBLE);
-
-            } else {
-                mBackgroundComplication.setImageDrawable(mDefaultAddComplicationDrawable);
-                mBackgroundComplicationBackground.setVisibility(View.INVISIBLE);
-            }
-        } else if (watchFaceComplicationId == mCenterComplicationId) {
+        if (watchFaceComplicationId == mCenterComplicationId) {
             if (complicationProviderInfo != null) {
                 mCenterComplication.setImageIcon(complicationProviderInfo.providerIcon);
                 mCenterComplicationBackground.setVisibility(View.VISIBLE);
-
             } else {
                 mCenterComplication.setImageDrawable(mDefaultAddComplicationDrawable);
                 mCenterComplicationBackground.setVisibility(View.INVISIBLE);
@@ -230,15 +142,11 @@ public class ComplicationConfigActivity extends Activity implements View.OnClick
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-
         if (requestCode == COMPLICATION_CONFIG_REQUEST_CODE && resultCode == RESULT_OK) {
 
             // Retrieves information for selected Complication provider.
-            ComplicationProviderInfo complicationProviderInfo =
-                    data.getParcelableExtra(ProviderChooserIntent.EXTRA_PROVIDER_INFO);
+            ComplicationProviderInfo complicationProviderInfo = data.getParcelableExtra(ProviderChooserIntent.EXTRA_PROVIDER_INFO);
             Log.d(TAG, "Provider: " + complicationProviderInfo);
-
             if (mSelectedComplicationId >= 0) {
                 updateComplicationViews(mSelectedComplicationId, complicationProviderInfo);
             }
