f6ae21dd71307958b0c4f267dd40e665ccb41dd7
Brad Martin <bradwaynemartin@gmail.com>
2019-11-20 18:31:06 -0600

    utils class

---
+++ apps/wear/watchfaces/SmartDriveDigital/app/src/main/java/com/permobil/smartdrivemx2digital/Utils.java
@@ -0,0 +1,15 @@
+package com.permobil.smartdrivemx2digital;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+class Utils {
+
+    public static String formatTwoDigitNumber(int hour) {
+        return String.format(Locale.getDefault(), "%02d", hour);
+    }
+
+    public static String getAmPmString(int amPm) {
+        return amPm == Calendar.AM ? "AM" : "PM";
+    }
+}
