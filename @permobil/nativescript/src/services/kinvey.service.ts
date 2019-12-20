0ace19b0ec0f8482c96ccab3ee00eaf838adbcde
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-20 10:30:26 -0600

    Fixed 'npm run setup.all.non-global' build

--- @permobil/nativescript/src/services/kinvey.service.ts
+++ @permobil/nativescript/src/services/kinvey.service.ts
@@ -290,42 +290,4 @@ export class KinveyService {
     });
     return this.handleResponse(response);
   }
-
-  async sendError(error: any, id?: string) {
-    this.checkAuth();
-    this.reformatForDb(error);
-    let response = null;
-    if (id) response = await this.put(KinveyService.api_error_db, error, id);
-    else response = await this.post(KinveyService.api_error_db, error);
-    return response;
-  }
-
-  async sendInfo(info: any, id?: string) {
-    this.checkAuth();
-    this.reformatForDb(info);
-    let response = null;
-    if (id) response = await this.put(KinveyService.api_info_db, info, id);
-    else response = await this.post(KinveyService.api_info_db, info);
-    return response;
-  }
-
-  async sendActivity(activity: any, id?: string) {
-    this.checkAuth();
-    this.reformatForDb(activity);
-    let response = null;
-    if (id)
-      response = await this.put(KinveyService.api_activity_db, activity, id);
-    else response = await this.post(KinveyService.api_activity_db, activity);
-    return response;
-  }
-
-  async sendSettings(settings: any, id?: string) {
-    this.checkAuth();
-    this.reformatForDb(settings);
-    let response = null;
-    if (id)
-      response = await this.put(KinveyService.api_settings_db, settings, id);
-    else response = await this.post(KinveyService.api_settings_db, settings);
-    return response;
-  }
 }
