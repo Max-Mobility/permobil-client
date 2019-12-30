1d18e7745c0115c5c897213c30ca80d54d873a88
William Emfinger <waemfinger@gmail.com>
2019-12-30 13:03:25 -0600

    closes #675 ensure that the date at the selected index matches the date of the object - prevent records older than the past week from showing up at the beginning of the week due to usage of "closestIndexTo" which returns 0 even if the dates do not match exactly.

--- apps/wear/smartdrive/app/pages/main-page/main-view-model.ts
+++ apps/wear/smartdrive/app/pages/main-page/main-view-model.ts
@@ -294,7 +294,7 @@ export class MainViewModel extends Observable {
           title: L('warnings.title.notice'),
           message: `${L('settings.paired-to-smartdrive')}\n\n${
             this.smartDrive.address
-          }`,
+            }`,
           okButtonText: L('buttons.ok')
         });
       }
@@ -797,7 +797,7 @@ export class MainViewModel extends Observable {
         okButtonText: L('buttons.ok')
       });
       try {
-        await requestPermissions(neededPermissions, () => {});
+        await requestPermissions(neededPermissions, () => { });
         // now that we have permissions go ahead and save the serial number
         this._updateSerialNumber();
       } catch (permissionsObj) {
@@ -1900,7 +1900,7 @@ export class MainViewModel extends Observable {
       .addCategory(android.content.Intent.CATEGORY_BROWSABLE)
       .addFlags(
         android.content.Intent.FLAG_ACTIVITY_NO_HISTORY |
-          android.content.Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET
+        android.content.Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET
       )
       .setData(android.net.Uri.parse(playStorePrefix + packageName));
     application.android.foregroundActivity.startActivity(intent);
@@ -1965,7 +1965,7 @@ export class MainViewModel extends Observable {
     }
     intent.addFlags(
       android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK |
-        android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+      android.content.Intent.FLAG_ACTIVITY_NEW_TASK
     );
     intent.addFlags(android.content.Intent.FLAG_ACTIVITY_NO_ANIMATION);
     application.android.foregroundActivity.startActivity(intent);
@@ -2478,8 +2478,9 @@ export class MainViewModel extends Observable {
           const obj = SmartDriveData.Info.loadInfo(...o);
           const objDate = new Date(obj.date);
           const index = closestIndexTo(objDate, dates);
-          // const usageDate = dates[index];
-          if (index > -1) {
+          const usageDate = dates[index];
+          const sameDay = isSameDay(usageDate, objDate);
+          if (index > -1 && sameDay) {
             usageInfo[index] = obj;
           }
         });
@@ -2487,7 +2488,7 @@ export class MainViewModel extends Observable {
       })
       .catch(err => {
         Sentry.captureException(err);
-        console.log('error getting recent info:', err);
+        console.error('error getting recent info:', err);
         return usageInfo;
       });
   }
