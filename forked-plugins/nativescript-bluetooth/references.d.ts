c2e243cf048f1840ced5b375ae0ad1b65765a756
Brad Martin <bradwaynemartin@gmail.com>
2020-01-06 09:10:08 -0600

    use ns-core modules to fix TSC

--- forked-plugins/nativescript-bluetooth/references.d.ts
+++ forked-plugins/nativescript-bluetooth/references.d.ts
@@ -1,3 +1,3 @@
-/// <reference path="../node_modules/tns-core-modules/nativescript-core.d.ts" />
+/// <reference path="../node_modules/@nativescript/core/nativescript-core.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android-23.d.ts" />
