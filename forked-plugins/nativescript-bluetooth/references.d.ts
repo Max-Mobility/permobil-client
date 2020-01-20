a13998304fe791309f0fe9bade8b5eb5600a6d8b
Brad Martin <bradwaynemartin@gmail.com>
2020-01-20 12:10:09 -0600

    clean up

--- forked-plugins/nativescript-bluetooth/references.d.ts
+++ forked-plugins/nativescript-bluetooth/references.d.ts
@@ -1,4 +1,2 @@
-// /// <reference path="./node_modules/@nativescript/core/nativescript-core.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/ios/ios.d.ts" />
-/// <reference path="./node_modules/tns-platform-declarations/ios/objc-x86_64/objc!CoreBluetooth.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android-23.d.ts" />
