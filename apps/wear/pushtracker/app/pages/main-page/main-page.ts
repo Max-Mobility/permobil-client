db3182e2c8e494786dcd1a2fcf513d363f7150c6
William Emfinger <waemfinger@gmail.com>
2020-02-05 09:19:59 -0600

    move code for less diff.

--- apps/wear/pushtracker/app/pages/main-page/main-page.ts
+++ apps/wear/pushtracker/app/pages/main-page/main-page.ts
@@ -4,8 +4,8 @@ import { MainViewModel } from './main-view-model';
 // Event handler for Page "navigatedTo" event attached in main-page.xml
 export function onNavigatedTo(args: EventData) {
   const page = args.object as Page;
-
   const vm = new MainViewModel();
+
   page.bindingContext = vm;
   vm.onMainPageLoaded(args);
 }
