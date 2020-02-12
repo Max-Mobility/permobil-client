8ee5c60bfeada3cfd85dcfb4808fe39cfb10b2a9
Brad Martin <bradwaynemartin@gmail.com>
2020-02-12 13:36:11 -0600

    wait function jsdoc comment

--- @permobil/core/src/utils/timing.util.ts
+++ @permobil/core/src/utils/timing.util.ts
@@ -1,3 +1,8 @@
+/**
+ * Simple function that allows you to `wait` for a set duration in milliseconds before this resolves.
+ * Can be useful in scenarios where you cannot use `async/await` - such as class constructors.
+ * @param msDuration [number] - The duration to wait in milliseconds.
+ */
 export const wait = (msDuration: number) => {
   return new Promise(resolve => setTimeout(resolve, msDuration));
 };
