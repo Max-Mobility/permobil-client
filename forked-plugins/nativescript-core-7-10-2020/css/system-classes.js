234ae22a6d7efc89d96819055507b5fd41fc97b0
Brad Martin <bradwaynemartin@gmail.com>
2020-07-21 13:58:58 -0500

    fix the ns-core ignores

---
+++ forked-plugins/nativescript-core-7-10-2020/css/system-classes.js
@@ -0,0 +1,43 @@
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.removeFromRootViewCssClasses = exports.pushToRootViewCssClasses = exports.getRootViewCssClasses = exports.getModalRootViewCssClass = exports.removeSystemCssClass = exports.pushToSystemCssClasses = exports.getSystemCssClasses = exports.ROOT_VIEW_CSS_CLASS = exports.MODAL_ROOT_VIEW_CSS_CLASS = exports.CLASS_PREFIX = void 0;
+var MODAL = 'modal';
+var ROOT = 'root';
+var cssClasses = [];
+exports.CLASS_PREFIX = 'ns-';
+exports.MODAL_ROOT_VIEW_CSS_CLASS = '' + exports.CLASS_PREFIX + MODAL;
+exports.ROOT_VIEW_CSS_CLASS = '' + exports.CLASS_PREFIX + ROOT;
+function getSystemCssClasses() {
+  return cssClasses;
+}
+exports.getSystemCssClasses = getSystemCssClasses;
+function pushToSystemCssClasses(value) {
+  cssClasses.push(value);
+  return cssClasses.length;
+}
+exports.pushToSystemCssClasses = pushToSystemCssClasses;
+function removeSystemCssClass(value) {
+  var index = cssClasses.indexOf(value);
+  var removedElement;
+  if (index > -1) {
+    removedElement = cssClasses.splice(index, 1);
+  }
+  return removedElement;
+}
+exports.removeSystemCssClass = removeSystemCssClass;
+function getModalRootViewCssClass() {
+  return exports.MODAL_ROOT_VIEW_CSS_CLASS;
+}
+exports.getModalRootViewCssClass = getModalRootViewCssClass;
+function getRootViewCssClasses() {
+  return __spreadArrays([exports.ROOT_VIEW_CSS_CLASS], cssClasses);
+}
+exports.getRootViewCssClasses = getRootViewCssClasses;
+function pushToRootViewCssClasses(value) {
+  return pushToSystemCssClasses(value) + 1;
+}
+exports.pushToRootViewCssClasses = pushToRootViewCssClasses;
+function removeFromRootViewCssClasses(value) {
+  return removeSystemCssClass(value);
+}
+exports.removeFromRootViewCssClasses = removeFromRootViewCssClasses;
+//# sourceMappingURL=system-classes.js.map
