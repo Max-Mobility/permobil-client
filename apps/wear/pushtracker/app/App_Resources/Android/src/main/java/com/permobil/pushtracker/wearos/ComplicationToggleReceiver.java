3eae59289970b0632e52203f11aad878bc291d0c
William Emfinger <waemfinger@gmail.com>
2020-10-01 13:28:25 -0500

    closes #846 check to see if the service is running and create the tap pending intent for the complication accordingly. if the service is running, simply update the complication; else open the pushtracker wear app. works for all complication providers in pt.w

--- apps/wear/pushtracker/app/App_Resources/Android/src/main/java/com/permobil/pushtracker/wearos/ComplicationToggleReceiver.java
+++ apps/wear/pushtracker/app/App_Resources/Android/src/main/java/com/permobil/pushtracker/wearos/ComplicationToggleReceiver.java
@@ -15,13 +15,19 @@
  */
 package com.permobil.pushtracker;
 
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.complications.ProviderUpdateRequester;
+import android.util.Log;
+
+import java.util.List;
 
 import com.permobil.pushtracker.Datastore;
 
@@ -53,6 +59,19 @@ public class ComplicationToggleReceiver extends BroadcastReceiver {
     requester.requestUpdate(complicationId);
   }
 
+  public static boolean isServiceRunning(Context context,Class<? extends Service> serviceClass){
+    final ActivityManager activityManager = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+    final List<RunningServiceInfo> services = activityManager.getRunningServices(Integer.MAX_VALUE);
+
+    for (RunningServiceInfo runningServiceInfo : services) {
+      Log.d("ComplicationToggleReceiver", String.format("Service:%s", runningServiceInfo.service.getClassName()));
+      if (runningServiceInfo.service.getClassName().equals(serviceClass.getName())){
+        return true;
+      }
+    }
+    return false;
+  }
+
   /**
    * Returns a pending intent, suitable for use as a tap intent, that causes a complication to be
    * toggled and updated.
@@ -62,13 +81,39 @@ public class ComplicationToggleReceiver extends BroadcastReceiver {
                                        ComponentName provider,
                                        int complicationId,
                                        String dataId) {
-    Intent intent = new Intent(context, ComplicationToggleReceiver.class);
-    intent.putExtra(EXTRA_PROVIDER_COMPONENT, provider);
-    intent.putExtra(EXTRA_COMPLICATION_ID, complicationId);
+    boolean isServiceRunning = isServiceRunning(context, ActivityService.class);
+    Log.w("ComplicationToggleReceiver", "getToggleIntent() isServiceRunning: " +
+          isServiceRunning);
 
-    // Pass complicationId as the requestCode to ensure that different complications get
-    // different intents.
-    return PendingIntent.getBroadcast(
-                                      context, complicationId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    Intent intent;
+    if (!isServiceRunning) {
+      // if the service is not running, we open the app to make sure
+      // the user can allow the permissions and such
+      intent = new Intent();
+      intent.setClassName(
+                          context,
+                          "com.permobil.pushtracker.MainActivity");
+      // A string containing the action name
+      intent.setAction(Constants.ACTION_START_SERVICE);
+      intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      // Pass complicationId as the requestCode to ensure that
+      // different complications get different intents.
+      return PendingIntent.getActivity(context,
+                                       complicationId,
+                                       intent,
+                                       PendingIntent.FLAG_UPDATE_CURRENT);
+    } else {
+      // else the service is already running, just try to update the
+      // complication
+      intent = new Intent(context, ComplicationToggleReceiver.class);
+      intent.putExtra(EXTRA_PROVIDER_COMPONENT, provider);
+      intent.putExtra(EXTRA_COMPLICATION_ID, complicationId);
+      // Pass complicationId as the requestCode to ensure that different
+      // complications get different intents.
+      return PendingIntent.getBroadcast(context,
+                                        complicationId,
+                                        intent,
+                                        PendingIntent.FLAG_UPDATE_CURRENT);
+    }
   }
 }
